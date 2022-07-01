5f5fe9695908cce0336f770be4fb66234c89d013
Joel Almeida <joel.almeida.sa@gmail.com>
2022-07-01 09:57:55 -0300

    feat(user service): implements create, update and remove methods

--- back-end/src/services/User.service.ts
+++ back-end/src/services/User.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { IUser, IUserUpdate } from 'src/interfaces/IUser';
 import { UserModel } from '../models/User.model';
 
 @Injectable()
 export class UserService {
   constructor(private _userModel: UserModel) {}
 
+  create(userData: IUser) {
+    return this._userModel.create(userData);
+  }
+
   getAll() {
     return this._userModel.getAll();
+  }
+
+  update(id: string, userData: IUserUpdate) {
+    return this._userModel.update(id, userData);
+  }
+
+  remove(id: string) {
+    return this._userModel.remove(id);
   }
 }
