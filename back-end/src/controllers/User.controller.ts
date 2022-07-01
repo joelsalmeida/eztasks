a5bfeebbf6d084d2afea5f788f0f8f515d1c3aae
Joel Almeida <joel.almeida.sa@gmail.com>
2022-07-01 09:58:13 -0300

    feat(user controller): implements create, update and remove methods

--- back-end/src/controllers/User.controller.ts
+++ back-end/src/controllers/User.controller.ts
@@ -1,12 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+
 import { UserService } from '../services/User.service';
+import { IUser, IUserUpdate } from '../interfaces/IUser';
 
 @Controller('users')
 export class UserController {
   constructor(private _userService: UserService) {}
 
+  @Post()
+  create(@Body() userData: IUser) {
+    return this._userService.create(userData);
+  }
+
   @Get()
   getAll() {
     return this._userService.getAll();
+  }
+
+  @Put(':id')
+  update(@Param() param: { id: string }, @Body() userData: IUserUpdate) {
+    return this._userService.update(param.id, userData);
+  }
+
+  @Delete(':id')
+  remove(@Param() param: { id: string }) {
+    return this._userService.remove(param.id);
   }
 }
