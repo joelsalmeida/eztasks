a82a1d71095e929bea5d2d410c5fdd3e1073ff0c
Joel Almeida <joel.almeida.sa@gmail.com>
2022-07-02 14:53:06 -0300

    feat(joi): creates todo schemas

---
+++ back-end/src/schemas/TodoSchemas.ts
@@ -0,0 +1,12 @@
+import Joi from 'joi';
+
+export const CreateTodoSchema = Joi.object({
+  userId: Joi.string().uuid().required(),
+  title: Joi.string().max(50).required(),
+  description: Joi.string().max(150),
+});
+
+export const UpdateTodoSchema = Joi.object({
+  title: Joi.string().max(50),
+  description: Joi.string().max(150),
+});
