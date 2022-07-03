8d36f4d0620c761a330445c4fa9c1139a4cb8729
Joel Almeida <joel.almeida.sa@gmail.com>
2022-07-03 15:54:41 -0300

    feat(todo): add status field

--- back-end/src/interfaces/ITodo.ts
+++ back-end/src/interfaces/ITodo.ts
@@ -2,10 +2,12 @@ export interface ITodo {
   id?: string;
   title: string;
   description?: string;
+  status: string;
   userId: string;
 }
 
 export interface ITodoUpdate {
   title?: string;
   description?: string;
+  status?: string;
 }
--- back-end/src/schemas/TodoSchemas.ts
+++ back-end/src/schemas/TodoSchemas.ts
@@ -4,9 +4,11 @@ export const CreateTodoSchema = Joi.object({
   userId: Joi.string().uuid().required(),
   title: Joi.string().max(50).required(),
   description: Joi.string().max(150),
+  status: Joi.string().required(),
 });
 
 export const UpdateTodoSchema = Joi.object({
   title: Joi.string().max(50),
   description: Joi.string().max(150),
+  status: Joi.string(),
 });
